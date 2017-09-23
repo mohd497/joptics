34a4f024f6497a42232a4b6d2e52df8a9f7f1823
ali <likesteel@gmail.com>
2017-09-23 18:27:28 +0500

    carriarwave s3

--- config/initializers/kaminari.rb
+++ config/initializers/kaminari.rb
@@ -2,8 +2,14 @@ Kaminari.configure do |config|
   config.page_method_name = :per_page_kaminari
 end
 
-module ActiveRecord
-  class Relation
-    alias_method :total_count, :count
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :total_count, :count
+      end
+    end
   end
 end
